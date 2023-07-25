d75c95571cc56c2a54af9915dd1b3b9ac6824969
Dapjzl <emmanueladedapo2019@gmail.com>
2023-07-25 20:05:50 +0100

    initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$username = 'root';
+$password = '';
+$hostname = 'localhost';
+$databasename='adms';
+$conn = mysqli_connect($hostname, $username, $password, $databasename);
+
+if($conn){
+	//echo 'successfully connected'. '<br>';
+
+}else{
+	die(mysqli_connect_error($conn));
+}
+
+
+?>
